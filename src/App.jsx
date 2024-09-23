804fca348e2b2227621093a6b5bb3067b9fd3845
Vatsalmadhur <bhartimadhur@gmail.com>
2024-09-23 15:43:39 +0530

    Folder Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Warning from "./components/common/Warning/Warning"
+
+
+function App() {
+
+  return (
+    <>
+    <Warning/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/common/Warning/Warning.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Warning = () => {
+  return (
+    <>
+    <div className='h-[50vh] w-full border-2 border-black flex items-center justify-center '>
+        <p className='text-2xl text-black'>This Section is currently in progress! Stay tuned.</p>
+    </div>
+    </>
+  )
+}
+
+export default Warning
